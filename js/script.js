278ba86892d15eb5be8bba9d58aefb6e918ee60e
Chet Hill <chetdhill@gmail.com>
2018-09-02 17:12:25 -0400

    Add JSON data

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
+function fetchJSON(callback) {
+    var obj = new XMLHttpRequest();
+    obj.overrideMimeType("application/json");
+    obj.open("GET", "https://api.myjson.com/bins/18esl0", true);
+    obj.onreadystatechange = function() {
+        if(obj.readyState == 4 && obj.status == "200") {
+            callback(obj.responseText);
+        }
+    };
+    obj.send(null);
+}
+
+function init() {
+    fetchJSON(function(response) {
+        var myJSON = JSON.parse(response);
+        console.log(response);
+    });
+}
+
 $(function(){
     $('.slider').click(function() {
-        $('.column2').css("outline", "1px solid red");
+        // $('.column2').css("outline", "1px solid red");
         console.log("Value: " + this.value);
+        $('.tickmarks p').removeClass('active');
+        init();
     })
 });
